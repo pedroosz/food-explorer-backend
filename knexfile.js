9b8467b900434f2fffe12b8d4c2c2d0469103a39
pedroosz <pedrodinp.70@gmail.com>
2023-06-18 14:56:27 -0300

    Fix: knex client error

--- knexfile.js
+++ knexfile.js
@@ -6,27 +6,73 @@ const path = require("path");
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-	development: {
-		client: "sqlite3",
-		connection: {
-			// eslint-disable-next-line no-undef
-			filename: path.resolve(__dirname, "src", "database", "data.db"),
-		},
-		pool: {
-			afterCreate: (conn, cb) => {
-				conn.run("PRAGMA foreign_keys = ON", cb);
-			},
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: path.resolve(
-				// eslint-disable-next-line no-undef
-				__dirname,
-				"src",
-				"database",
-				"knex",
-				"migrations"
-			),
-		},
-	},
+  development: {
+    client: "sqlite3",
+    connection: {
+      // eslint-disable-next-line no-undef
+      filename: path.resolve(__dirname, "src", "database", "data.db"),
+    },
+    pool: {
+      afterCreate: (conn, cb) => {
+        conn.run("PRAGMA foreign_keys = ON", cb);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        // eslint-disable-next-line no-undef
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+  },
+  staging: {
+    client: "sqlite3",
+    connection: {
+      // eslint-disable-next-line no-undef
+      filename: path.resolve(__dirname, "src", "database", "data.db"),
+    },
+    pool: {
+      afterCreate: (conn, cb) => {
+        conn.run("PRAGMA foreign_keys = ON", cb);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        // eslint-disable-next-line no-undef
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+  },
+  production: {
+    client: "sqlite3",
+    connection: {
+      // eslint-disable-next-line no-undef
+      filename: path.resolve(__dirname, "src", "database", "data.db"),
+    },
+    pool: {
+      afterCreate: (conn, cb) => {
+        conn.run("PRAGMA foreign_keys = ON", cb);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        // eslint-disable-next-line no-undef
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+  },
 };
