2652c88477498be2dab1016e0a4f952e0a2e98a0
pedroosz <pedrodinp.70@gmail.com>
2023-06-18 15:28:41 -0300

    Fix: Add necessary initial data and env example for jwt secret

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   jwt: {
-    secret: "4c8EAjZd8VRAFj8!X%Qq&g^iypk&4&F56N47NL2c*KF6i9vhmm*^q&!fvkr26L7$MvfXXmB^G&x&oXyLe#^6h5A^$DT&d#NKn77bnWASQMDX8&@YWb6$*SknzvASy4!M",
-    expire: "1d"
-  }
-}
+    secret: process.env.JWT_AUTH_SECRET || "development secret",
+    expire: "1d",
+  },
+};
